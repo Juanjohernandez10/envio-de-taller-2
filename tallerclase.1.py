454ae6043fdcbaafde20e16cfb8b230dbbde429a
Juanjohernandez10 <159075340+Juanjohernandez10@users.noreply.github.com>
2024-03-01 10:23:20 -0500

    Update tallerclase.1.py

--- tallerclase.1.py
+++ tallerclase.1.py
@@ -10,9 +10,9 @@ var_contador = 0
 while var_contador < var_cantidad_numeros:
     numero = float(input('Ingresa un número: '))
     
-    if 10<= numero <= 20:
+    if numero >= 10 and numero <= 20:
         var_numeros_en_rango += 1
     
     var_contador += 1
 
-print(f"\nCantidad de números en el rango de 10-20: {var_numeros_en_rango}")
+print('\nCantidad de números en el rango de 10-20:', var_numeros_en_rango)
